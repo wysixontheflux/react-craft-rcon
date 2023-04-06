5daf6d2f46c894c0ce23f7beda1a8540b1d26f3d
Enzo Deshayes <enzodeshayes@Mac-mini-de-Enzo.local>
2023-04-06 23:30:13 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [list, setList] = useState('');
+
+  const handleClick = async () => {
+    try {
+      const response = await fetch('http://localhost:3001/list');
+      const data = await response.text();
+      setList(data);
+    } catch (err) {
+      console.error(err);
+      setList('Erreur lors de l\'exécution de la commande');
+    }
+  }
+
+  const handleClickGIve = async () => {
+    try {
+      const response = await fetch('http://localhost:3001/give');
+      const data = await response.text();
+      setList(data);
+    } catch (err) {
+      console.error(err);
+      setList('Erreur lors de l\'exécution de la commande');
+    }
+  }
+
+  const handleClickGIveGold = async () => {
+    try {
+      const response = await fetch('http://localhost:3001/givegold');
+      const data = await response.text();
+      setList(data);
+    } catch (err) {
+      console.error(err);
+      setList('Erreur lors de l\'exécution de la commande');
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <button onClick={handleClick}>Liste des joueurs</button>
+        <p>{list}</p>
+        <button onClick={handleClickGIve}>give 64 diamonds</button>
+        <button onClick={handleClickGIveGold}>give 64 gold</button>
+      </div>
   );
 }
 
